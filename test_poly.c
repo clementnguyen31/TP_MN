2311633a9cf2215961af9775e7e5fbc07784bcf5
Clément Nguyen <clementnguyen31@gmail.com>
2021-02-12 22:15:10 +0100

    amelioration test poly pleins

--- test_poly.c
+++ test_poly.c
@@ -3,28 +3,46 @@
 
 #include "poly.h"
 
-
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-  p_polyf_t p1, p2 ;
-  
+  p_polyf_t p1, p2;
+
   if (argc != 3)
-    {
-      fprintf (stderr, "deux paramètres (polynomes,fichiers) sont à passer \n") ;
-      exit (-1) ;
-    }
-      
-  p1 = lire_polynome_float (argv [1]) ;
-  p2 = lire_polynome_float (argv [2]) ;
+  {
+    fprintf(stderr, "deux paramètres (polynomes,fichiers) sont à passer \n");
+    exit(-1);
+  }
 
-  ecrire_polynome_float (p1) ;
-  printf("\n");
-  ecrire_polynome_float (p2) ;
-  printf("\n");
+  p1 = lire_polynome_float(argv[1]);
+  p2 = lire_polynome_float(argv[2]);
 
-  p_polyf_t p3 = puissance_polynome(p1,3);
+  printf("Polynome 1 : ");
+  ecrire_polynome_float(p1);
+
+  printf("Polynome 2 : ");
+  ecrire_polynome_float(p2);
+
+  p_polyf_t p3 = NULL;
+
+  egalite_polynome(p1, p2) ? printf("P1 et P2 sont egaux\n") : printf("P1 et P2 ne sont pas egaux\n");
+
+  p3 = addition_polynome(p1, p2);
+  printf("Addition entre P1 et P2 : ");
   ecrire_polynome_float(p3);
- 
-  p_polyf_t p4 = composition_polynome(p1,p2);
-  ecrire_polynome_float(p4);
+
+  p3 = multiplication_polynome_scalaire(p1, 3);
+  printf("Multiplication Scalaire P1 et 3 : ");
+  ecrire_polynome_float(p3);
+
+  p3 = multiplication_polynomes(p1, p2);
+  printf("Multiplication entre P1 et P2 : ");
+  ecrire_polynome_float(p3);
+
+  p3 = puissance_polynome(p1, 3);
+  printf("P1 Puissance 3 : ");
+  ecrire_polynome_float(p3);
+
+  p3 = composition_polynome(p1, p2);
+  printf("Composition entre P1 et P2 : ");
+  ecrire_polynome_float(p3);
 }
